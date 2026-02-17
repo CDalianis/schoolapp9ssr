6a89d16159e314a354cf13991046cd43f4e65863
CDalianis <christos_dl@hotmail.com>
2026-02-17 22:22:03 +0200

    feat: authentication concern 2

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,10 +1,9 @@
 package gr.aueb.cf.schoolapp.mapper;
 
-import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherEditDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
-import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.*;
+import gr.aueb.cf.schoolapp.model.Role;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.model.User;
 import gr.aueb.cf.schoolapp.model.static_data.Region;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +12,11 @@ public class Mapper {
 
     public Teacher mapToTeacherEntity(TeacherInsertDTO teacherInsertDTO) {
         return new Teacher(null, null, teacherInsertDTO.vat(), teacherInsertDTO.firstname(), teacherInsertDTO.lastname(), null);
+    }
+
+    public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
+        return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
+                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
     }
 
     public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
@@ -24,8 +28,15 @@ public class Mapper {
         return new RegionReadOnlyDTO(region.getId(), region.getName());
     }
 
-    public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
-        return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
-                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(userInsertDTO.username(), userInsertDTO.password());
+    }
+
+    public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
+        return new UserReadOnlyDTO(user.getUuid().toString(), user.getUsername(), user.getRole().getName());
+    }
+
+    public RoleReadOnlyDTO mapToRoleReadOnlyDTO(Role role) {
+        return new RoleReadOnlyDTO(role.getId(), role.getName());
     }
 }
